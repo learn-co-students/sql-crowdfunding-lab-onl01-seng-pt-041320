97a9e89b9bb629889dec8c95359cb3829f1d36c3
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-09 18:16:16 -0700

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   query = 'SELECT projects.title, SUM(pledges.amount) FROM projects 
            LEFT OUTER JOIN pledges
-           ON projects.id = pledges.id
+           ON projects.id = pledges.project_id
            GROUP BY projects.title;'
   query
 end
